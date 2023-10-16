1be957acd88ff2b44603734bedaf573c0b449a36
suka1557 <suka1557@gmail.com>
2023-10-16 20:10:48 +0530

    created a yaml reader function to read config from yaml files and return as dict

---
+++ utils/reader.py
@@ -0,0 +1,29 @@
+import os
+import sys
+PROJECT_ROOT = os.path.abspath("./")
+sys.path.append(PROJECT_ROOT)
+import yaml
+
+from ensure import ensure_annotations
+
+@ensure_annotations
+def read_yaml(file_path: str) -> dict:
+    """
+    Function to read data from YAML file and return data in form of a dict
+
+    Arguments:
+        file_path - str: location of the YAML file
+
+    Returns:
+        file_data - dict: dictionary containing key value pairs of items read from the file
+
+    """
+
+    with open(file_path, 'r') as file:
+        file_data = yaml.safe_load(file)
+
+    return file_data
+
+if __name__ == '__main__':
+    file_data = ( read_yaml(os.path.join(PROJECT_ROOT, 'config.yaml')) )
+    print(type(file_data['MAINTAIN_CLASS_BALANCE']))
