487f60b952979e7a4101c33c698daf17b4806e41
suka1557 <suka1557@gmail.com>
2023-10-17 22:29:06 +0530

    tested model loading,reading image and making predictions on local file

--- scripts/test_image_parsing.py
+++ scripts/test_image_parsing.py
@@ -8,6 +8,7 @@ filename = '/Users/sukant.kumar/Downloads/test_image.png'
 
 import cv2 
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Load sample image
 test_image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
@@ -18,6 +19,19 @@ img_resized = cv2.resize(test_image, (28, 28), interpolation=cv2.INTER_LINEAR)
 img_resized = cv2.bitwise_not(img_resized)
 img_array = (img_resized.flatten())
 print(img_array.shape)
-img_array  = img_array.reshape(-1,1).T
-print(img_array.shape)
+img_array  = pd.DataFrame( img_array.reshape(-1,1).T )
+img_array.columns = ['pixel'+str(i) for i in range(784)]
+
+#Load models
+from src.evaluation.model_loader import load_rf_model_and_pca_decomposer
+from src.evaluation.model_prediction import get_predictions
+
+
+#Load model in global memory
+pca, model = load_rf_model_and_pca_decomposer()
+print("Loaded model from s3 bucket")
+
+prediction = get_predictions(img_array, pca, model)
+print(type(prediction))
+
 
