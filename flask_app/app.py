5b85ed695aa8f8d77042799f1173b897b64106a5
suka1557 <suka1557@gmail.com>
2023-10-17 22:31:55 +0530

    removed debug flag from flask app run

--- flask_app/app.py
+++ flask_app/app.py
@@ -28,4 +28,4 @@ def identify_digit():
     return jsonify({"Digit in Image": prediction})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000)
