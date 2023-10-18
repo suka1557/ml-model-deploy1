ca9969bb1b5b89899feef90bf57ea592b9fa1b5a
suka1557 <suka1557@gmail.com>
2023-10-18 22:15:30 +0530

    added logging to model training pipeline

--- src/pipelines/random_forest_training_pipeline.py
+++ src/pipelines/random_forest_training_pipeline.py
@@ -2,26 +2,18 @@ import os
 import sys
 PROJECT_ROOT = os.path.abspath("./")
 sys.path.append(PROJECT_ROOT)
-from dotenv import load_dotenv
+from utils.logger import logger
 from src.data_processing.data_loader import DataLoader
 from src.feature_engineering.feature_scaler import ScaleInputs
 from src.model_training.model_random_forest_experiment import random_forest_model_experiments
 from src.experiment_track.mlflow_extract_results import get_best_experiment_details
 from utils.reader import read_yaml
 
-load_dotenv(os.path.join(PROJECT_ROOT, 'config.env'))
-load_dotenv(os.path.join(PROJECT_ROOT, 'secrets.env'))
 configs = read_yaml(os.path.join(PROJECT_ROOT, 'config.yaml'))
 
 #PARAMETERS TO TRY
 # NO_COMPONENTS, NO_ESTIMATORS
 hyperparameters_dict = configs['parameter_dict']
-# {
-#     "no_components": [50,60,80],
-#     "n_estimators": [20,50, 100],
-#     "max_depth":[2,5,8],    
-# }
-
 
 #SET UP TRACKING
 EXPERIMENT_NAME = configs['EXPERIMENT_NAME']
@@ -55,7 +47,7 @@ if __name__ == '__main__':
                                     test_set_size=TRAIN_VALID_SPLIT_RATIO,
                                     class_distributed=MAINTAIN_CLASS_BALANCE)
     
-    print('Completed Experiments for all parameter combinations using MLFlow')
+    logger.info('Completed Experiments for all parameter combinations using MLFlow')
     
     #Get best parameters for this experiment to be run on mlflow
     best_parameters_dict = get_best_experiment_details(experiment_name=EXPERIMENT_NAME, evaluation_criteria=EVALUATION_SCORE)
@@ -70,7 +62,7 @@ if __name__ == '__main__':
 
     #converting single values into iterables lists, which need to bes paased to itertools while doing model fitting
     best_parameters_dict = {k:[v] for k,v in best_parameters_dict.items()}
-    print(f"Best Parameters: {best_parameters_dict}")
+    logger.info(f"Best Parameters found from all the runs : {best_parameters_dict}")
 
     # Train a model with best parameters and store the model to S3 bucket
     FINAL_EXPERIMENT_NAME = 'FINAL_' + EXPERIMENT_NAME
@@ -82,7 +74,7 @@ if __name__ == '__main__':
                                     class_distributed=MAINTAIN_CLASS_BALANCE,
                                     log_model=True)
     
-    print("Trained final model with best parameter combination and saved model to S3 bucket")
+    logger.info("Trained final model with best parameter combination and saved model to S3 bucket")
 
     
 
